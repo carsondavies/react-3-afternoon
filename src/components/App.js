5baa371c8c43295a117138d846ac49c973bd1031
Carson Davies <carsondavies@Carsons-MBP.T-mobile.com>
2020-08-21 19:32:23 -0600

    initial commit react-3-afternoon done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
+
+const basicURL = 'https://practiceapi.devmountain.com/api'
 
 class App extends Component {
   constructor() {
@@ -13,25 +16,41 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios.get(`${basicURL}/posts`).then(res => {
+      this.setState({
+        posts: res.data
+      })
+    })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`${basicURL}/posts?id=${id}`, { text }).then(res => {
+      this.setState({
+        posts: res.data
+      })
+    })
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`${basicURL}/posts?id=${id}`).then(res => {
+      this.setState({
+        posts: res.data
+      })
+    })
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post(`${basicURL}/posts`, { text }).then(res => {
+      this.setState({
+        posts: res.data
+      })
+    })
   }
 
   render() {
@@ -43,8 +62,20 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose createPostFn={this.createPost} />
+
+          {
+            posts.map(post => (
+              <Post key={post.id}
+                text={post.text}
+                date={post.date}
+                id={post.id}
+                updatePostFn={this.updatePost}
+                deletePostFn={this.deletePost}
+              />
+            ))
+          }
+
         </section>
       </div>
     );
